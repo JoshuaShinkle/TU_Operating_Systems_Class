2a0c43d914eeca6b37fb9667311dc13c8dfc81a4
Joshua Shinkle <joshua_shinkle@taylor.edu>
2023-02-21 02:19:41 +0000

    Complete truncating redirection

--- shell.c
+++ shell.c
@@ -6,6 +6,9 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char* argv[], char* envp[])
 {
@@ -48,6 +51,28 @@ int main(int argc, char* argv[], char* envp[])
             token_number++;		
         }
         
+        // int fd[2];
+        // char pipe[] = "|";
+        int pipe_check = 0;
+
+	    // if(strcmp(array_for_exec[1], pipe)==0){
+        //     pipe_check = 1;
+        //     pipe(fd);
+        // }
+
+        bool performing_truncating_redirection = false;
+        int index_in_array_for_exec_of_redirect_filename = -1;
+        int index_in_array_for_exec_of_redirect_symbol = -1;
+        int size_of_array_for_exec_without_redirect_symbol_or_filename = 0;
+        for (int i=0; i<token_number-1; i++) {
+            if (strcmp(array_for_exec[i], ">")==0){
+                performing_truncating_redirection = true;
+                index_in_array_for_exec_of_redirect_filename = i+1;
+                index_in_array_for_exec_of_redirect_symbol = i;
+                size_of_array_for_exec_without_redirect_symbol_or_filename = i;
+            }
+        }
+
         if (strcmp(command_for_exec, "cd") == 0) {
             int return_value = chdir(array_for_exec[1]);
             if (return_value == -1) {
@@ -61,10 +86,45 @@ int main(int argc, char* argv[], char* envp[])
                     fprintf(stderr, "Yoiks!\n");
                     exit(1);
                 case 0:
-                    int return_value = execvp(command_for_exec, array_for_exec);
-                    if (return_value == -1) {
-                        printf("Invalid command!\n");
-                        exit(1);
+                    if(pipe_check){
+                        // dup2(fd[1], 1);
+                        // close(fd[0]);
+                        // close(fd[1]);
+                        // execvp(command_for_exec, array_for_exec[2]);
+                    } else if (performing_truncating_redirection) {
+                        int redir_fd;
+
+                        redir_fd = open( array_for_exec[index_in_array_for_exec_of_redirect_filename], O_CREAT|O_WRONLY|O_TRUNC, 0777 );
+                        if( redir_fd == -1 ) {
+                            printf("error openning file.\n");
+                            return 2;
+                        }
+
+                        if( dup2( redir_fd, 1 ) == -1 ) {
+                            printf("error performing dup2.\n");
+                            return 3;
+                        }
+
+                        close( redir_fd );
+                        
+                        char* array_for_exec_without_redirect_symbol_or_filename[size_of_array_for_exec_without_redirect_symbol_or_filename+1];  //+1 to leave room for \0 at end of array
+                        for (int i=0; i<size_of_array_for_exec_without_redirect_symbol_or_filename; i++) {
+                            array_for_exec_without_redirect_symbol_or_filename[i] = array_for_exec[i];
+                        }
+
+                        array_for_exec_without_redirect_symbol_or_filename[size_of_array_for_exec_without_redirect_symbol_or_filename] = '\0';
+                        
+                        int return_value = execvp(command_for_exec, array_for_exec_without_redirect_symbol_or_filename);
+                        if (return_value == -1) {
+                            printf("Invalid command!\n");
+                            exit(1);
+                        }
+                    } else {
+                        int return_value = execvp(command_for_exec, array_for_exec);
+                        if (return_value == -1) {
+                            printf("Invalid command!\n");
+                            exit(1);
+                        }
                     }
                     exit(0);
                 default:  
